acfac15fa3a473f914735514822d6d89dfc1d962
MrValdez <shunyvaldez@gmail.com>
2015-07-04 14:01:15 +0800

    added velocity

--- main.py
+++ main.py
@@ -23,11 +23,25 @@ class GameObject:
 class Hero(GameObject):
     def __init__(self):
         GameObject.__init__(self, 'snake.png')
+        self.velocity = [0, 0]
         
     def update(self):
-        self.pos[0] += 1
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.velocity[0] -= 1 / 4
+        if keystate[pygame.K_RIGHT]:
+            self.velocity[0] += 1 / 4
+            
+        self.pos[0] += self.velocity[0]
+            
+class Enemy(GameObject):
+    def __init__(self):
+        GameObject.__init__(self, 'moongoose.png')
+        self.pos[0] = 500
+        
 
 player1 = Hero()
+enemy = Enemy()
 
 while True:
     surface.fill([255, 255, 255])
@@ -41,6 +55,11 @@ while True:
     if keystate[pygame.K_ESCAPE]:
         pygame.quit()
 
+    player1.update()
+    enemy.update()
+    
     player1.draw(surface)
+    #enemy.draw(surface)
+    
     pygame.display.update()
     clock.tick(60)
