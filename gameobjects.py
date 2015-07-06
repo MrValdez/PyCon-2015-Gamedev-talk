b489ca50925085baee5eddf6c2f0f3998388eeae
MrValdez <shunyvaldez@gmail.com>
2015-07-06 09:52:48 +0800

    added simple component to game objects

--- gameobjects.py
+++ gameobjects.py
@@ -1,5 +1,12 @@
 import pygame
 
+# functions are used for simplicity and demo purposes. 
+# It has problems such as making assumptions that the game object has the
+# appropriate variables.
+# Assignment: use your favorite design pattern
+def component_Gravity(GameObject):
+    GameObject.velocity[1] -= 1
+
 class GameObject:
     def __init__(self, image):
         image = pygame.image.load(image).convert()
@@ -8,9 +15,12 @@ class GameObject:
 
         self.image = image
         self.pos = pos
+        
+        self.components = []
 
     def update(self, GameState):
-        pass
+        for component in self.components:
+            component(self)
         
     def draw(self, surface):
         surface.blit(self.image, self.pos)
@@ -19,6 +29,8 @@ class Hero(GameObject):
     def __init__(self):
         GameObject.__init__(self, 'snake.png')
         self.velocity = [0, 0]
+        self.components.append(component_Gravity)
+        
         
     def update(self, GameState):
         keystate = pygame.key.get_pressed()
@@ -29,7 +41,11 @@ class Hero(GameObject):
         if keystate[pygame.K_RIGHT]:
             self.velocity[0] += 1 / 4
             
+        GameObject.update(self, GameState)
+        
         self.pos[0] += self.velocity[0]
+        self.pos[1] += self.velocity[1]
+        
             
 class Enemy(GameObject):
     def __init__(self):
