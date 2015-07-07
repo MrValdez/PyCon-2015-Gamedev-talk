bf6b3e4819c30644ac1a0c755c7f6885f034c8bc
MrValdez <shunyvaldez@gmail.com>
2015-07-07 10:42:16 +0800

    added collision check to component. Renamed collision component to collidable (naming convention is simply preferences)

--- gameobjects.py
+++ gameobjects.py
@@ -7,10 +7,19 @@ def component_Movement(GameObject, GameState):
     GameObject.pos[0] += GameObject.velocity[0]
     GameObject.pos[1] += GameObject.velocity[1]
 
-def component_Collision(GameObject, GameState):
-    for GO in GameState.GameObjects:
+def component_Collidable(GameObject, GameState):
+    for target in GameState.GameObjects:
+        if target == GameObject:
+            # do not perform collision detection against the same object
+            continue
+            
         box1 = GameObject.image.get_rect()
-        box2 = GO.image.get_rect()
+        box1 = box1.move(GameObject.pos)
+        box2 = target.image.get_rect()
+        box2 = box2.move(target.pos)
+
+        if box1.colliderect(box2):
+            print("collision")
 
 class GameObject:
     def __init__(self, image):
@@ -36,7 +45,7 @@ class Hero(GameObject):
         self.velocity = [0, 0]
         self.components.append(component_Gravity)        
         self.components.append(component_Movement)        
-        self.components.append(component_Collision)        
+        self.components.append(component_Collidable)        
         
     def update(self, GameState):
         keystate = pygame.key.get_pressed()
